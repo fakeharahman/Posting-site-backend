42a95518be41b64f7ac4c312bb1cb938fd56f945
fakeharahman <fakeha126@gmail.com>
2021-08-08 20:50:05 +0530

    connect db

---
+++ config/db.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    HOST: "us-cdbr-east-04.cleardb.com",
+    USER: "b0e9b2a1b7aee8",
+    PASSWORD: "2769f77c",
+    DB: "heroku_5b5c5d49c0be57d"
+  };
+
+  //mysql://b0e9b2a1b7aee8:2769f77c@us-cdbr-east-04.cleardb.com/heroku_5b5c5d49c0be57d?reconnect=true
--- util/database.js
+++ util/database.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("chat-app", "root", "Mysql123", {
+const dbConfig=require("../config/db.config")
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   dialect: "mysql",
-  host: "chat-app-db.cvnrfsaugc6h.us-east-2.rds.amazonaws.com",
-  port:"3306"
+  host: dbConfig.HOST,
+  // port:"3306"
 });
+// const sequelize = new Sequelize("chat-app", "root", "Mysql123", {
+//   dialect: "mysql",
+//   host: "chat-app-db.cvnrfsaugc6h.us-east-2.rds.amazonaws.com",
+//   port:"3306"
+// });
 
 module.exports = sequelize;
